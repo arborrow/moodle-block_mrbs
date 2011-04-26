0551d5361a167c2fd0cc970b7f8d626d2ec4e429
Davo Smith <git@davosmith.co.uk>
2011-04-26 19:30:03 +0100

    Added GPL text and removed 'ID' tag

--- block_mrbs.php
+++ block_mrbs.php
@@ -1,4 +1,19 @@
-<?php // $Id: block_mrbs.php,v 1.11 2010/01/16 13:08:16 arborrow Exp $
+<?php
+
+// This file is part of the MRBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 class block_mrbs extends block_base {
 
