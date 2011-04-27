922becbdfaa60a597f6b60136cfdd5a61d745c58
Davo Smith <git@davosmith.co.uk>
2011-04-27 17:20:05 +0100

    Update index.php: gpl, moodle DB access, moodle_url

--- web/index.php
+++ web/index.php
@@ -1,12 +1,27 @@
 <?php
 
-# $Id: index.php,v 1.3 2009/12/27 19:15:18 arborrow Exp $
+// This file is part of the MRBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-# Index is just a stub to redirect to the appropriate view
-# as defined in config.inc.php using the variable $default_view
-# If $default_room is defined in config.inc.php then this will
-# be used to redirect to a particular room.
-require_once("../../../config.php");; //for Moodle integration
+/*
+  Index is just a stub to redirect to the appropriate view
+  as defined in config.inc.php using the variable $default_view
+  If $default_room is defined in config.inc.php then this will
+  be used to redirect to a particular room.
+*/
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php'); //for Moodle integration
 require_once "grab_globals.inc.php";
 include("config.inc.php");
 include("$dbsys.php");
@@ -19,28 +34,25 @@ $year  = date("Y");
 switch ($default_view)
 {
 	case "month":
-		$redirect_str = "month.php?year=$year&month=$month";
+        //UT
+		$redirect = new moodle_url('/blocks/mrbs/month.php', array('year'=>$year, 'month'=>$month));
 		break;
 	case "week":
-		$redirect_str = "week.php?year=$year&month=$month&day=$day";
+        //UT
+		$redirect = new moodle_url('/blocks/mrbs/week.php', array('year'=>$year, 'month'=>$month, 'day'=>$day));
 		break;
 	default:
-		$redirect_str = "day.php?day=$day&month=$month&year=$year";
+        //UT
+        $redirect = new moodle_url('/blocks/mrbs/day.php', array('day'=>$day, 'month'=>$month, 'year'=>$year));
 }
 
 if (!empty($default_room)) {
+    //UT
 	$sql = "select area_id from $tbl_room where id=$default_room";
-	$res = sql_query($sql);
-	if ($res) {
-		if( sql_count($res) == 1 ) {
-			$row = sql_row($res, 0);
-			$area = $row[0];
-			$room = $default_room;
-			$redirect_str .= "&area=$area&room=$room";
-		}
+	$res = $DB->get_record('mrbs_room', 'area_id', array('id'=>$default_room));
+	if (!empty($res)) {
+        $redirect->params(array('area'=>$res->area_id, 'room'=>$default_room));
 	}
 }
 
-header("Location: $redirect_str");
-
-?>
+redirect($redirect);
