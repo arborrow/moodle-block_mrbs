540a7dc54ad4aefeaddc7539e41d891a73362b9e
Davo Smith <git@davosmith.co.uk>
2011-04-28 17:34:18 +0100

    Added GPL, slight code tidy

--- web/auth_moodle.php
+++ web/auth_moodle.php
@@ -1,9 +1,24 @@
 <?php
 
-/* $Id: auth_moodle.php,v 1.2 2010/01/16 15:15:57 arborrow Exp $
+// This file is part of the MRBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
  *
  * Assigns MRBS access levels based on the user status
- * within your Moodle installation. 
+ * within your Moodle installation.
  *
  *
  * MRBS Levels
@@ -11,7 +26,7 @@
  * 1 - View and make bookings
  * 2 - Full administration - add rooms and bookings
  *
- * Moodle Integration 
+ * Moodle Integration
  * Defines one of the above levels to admins, teachers
  * students, creators, and guests seperately.
  *
@@ -25,17 +40,15 @@
  *
  */
 
-require_once("../../../config.php");
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php'); //for Moodle integration
 
 function authValidateUser($user, $pass) {
     return 1;
 }
 
 function authGetUserLevel($user, $lev1_admin) {
-    global $USER;
-
     // HACK For Moodle 1.7 With Roles Block...
-    $context = get_context_instance(CONTEXT_SYSTEM, SITEID);  
+    $context = get_context_instance(CONTEXT_SYSTEM, SITEID);
 
     // Set Access leve for users via MRBS block and Moodle 1.7 roles
     if (has_capability('block/mrbs:administermrbs', $context)) {
