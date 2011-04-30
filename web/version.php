d997e5f28a2a4179f76a675d87c1ab185e340d62
Davo Smith <git@davosmith.co.uk>
2011-04-30 09:33:27 +0100

    Added GPL, removed Moodle config import (as not used)

--- web/version.php
+++ web/version.php
@@ -1,11 +1,26 @@
 <?php
 
-# This should probably be a class, but you can only have protected
-# class members in PHP 5, so we won't bother
-require_once("../../../config.php"); //for Moodle integration
+// This file is part of the MRBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// This should probably be a class, but you can only have protected
+// class members in PHP 5, so we won't bother
+
 function get_mrbs_version()
 {
-  # MRBS developers, make sure to update this string before each release
+  // MRBS developers, make sure to update this string before each release
   $mrbs_version = "MRBS 1.2.5";
 
   return $mrbs_version;
