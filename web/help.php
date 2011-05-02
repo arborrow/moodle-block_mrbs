b0f7e2fb8377e05c32a885c23604537e789b85a0
Davo Smith <git@davosmith.co.uk>
2011-05-02 10:12:33 +0100

    Added GPL, PAGE->url, removed unavailable database version number, fixed fallback FAQ location

--- web/help.php
+++ web/help.php
@@ -1,12 +1,25 @@
 <?php
 
-# $Id: help.php,v 1.9 2009/06/10 15:19:35 mike1989 Exp $
-require_once("../../../config.php"); //for Moodle integration
-require_once "grab_globals.inc.php";
+// This file is part of the MRBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
 include "config.inc.php";
-include "$dbsys.php";
 include "functions.php";
 include "version.php";
+
 global $USER;
 
 if ($CFG->forcelogin) {
@@ -15,24 +28,32 @@ if ($CFG->forcelogin) {
 
 $day = optional_param('day', 0, PARAM_INT);
 $month = optional_param('month', 0, PARAM_INT);
-$year = optional_param('year', 0, PARAM_INT); 
-$area = optional_param('area', get_default_area(),  PARAM_INT);
+$year = optional_param('year', 0, PARAM_INT);
+$area = optional_param('area', 0,  PARAM_INT);
 
-#If we dont know the right date then make it up
+//If we dont know the right date then make it up
 if(($day==0) or ($month==0) or ($year==0))
 {
 	$day   = date("d");
 	$month = date("m");
 	$year  = date("Y");
 }
-//if(empty($area)) - using optional_param -ab
-//	$area = get_default_area();
+
+$thisurl = new moodle_url('/blocks/mrbs/web/help.php', array('day'=>$day, 'month'=>$month, 'year'=>$year));
+if ($area > 0) {
+    $thisurl->param('area', $area);
+} else {
+    $area = get_default_area();
+}
+
+$PAGE->set_url($thisurl);
+require_login();
 
 print_header_mrbs($day, $month, $year, $area);
 
 echo "<H3>" . get_string('about_mrbs','block_mrbs') . "</H3>\n";
 echo "<P><a href=\"http://mrbs.sourceforge.net\">".get_string('mrbs','block_mrbs')."</a> - ".get_mrbs_version()."\n";
-echo "<BR>" . get_string('database','block_mrbs') . sql_version() . "\n";
+//echo "<BR>" . get_string('database','block_mrbs') . sql_version() . "\n";
 echo "<BR>" . get_string('system','block_mrbs') . php_uname() . "\n";
 echo "<BR>PHP: " . phpversion() . "\n";
 
@@ -44,8 +65,8 @@ echo get_string('please_contact','block_mrbs') . '<a href="mailto:' . $mrbs_admi
 if (file_exists($CFG->wwwroot.'blocks/mrbs/lang/'.$USER->lang.'/help/site_faq.html')) {
     include $CFG->wwwroot.'blocks/mrbs/lang/'.$USER->lang.'/help/site_faq.html';
 } else {
-    include $CFG->wwwroot.'blocks/mrbs/lang/en_utf8/help/site_faq.html';
+    include $CFG->wwwroot.'blocks/mrbs/lang/en/help/site_faq.html';
 }
 
 include "trailer.php";
-?>
+
