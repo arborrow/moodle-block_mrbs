d8e50dca574826c4e5e1fd8616e492127c83b7f8
Davo Smith <git@davosmith.co.uk>
2012-04-10 13:31:17 +0100

    MRBS block - fixed error when creating default roles during install

--- db/install.php
+++ db/install.php
@@ -29,23 +29,23 @@ function xmldb_block_mrbs_install() {
     // Create the viewer role
     $mrbsviewerid = create_role(get_string('mrbsviewer', 'block_mrbs'), 'mrbsviewer', get_string('mrbsviewer_desc', 'block_mrbs'));
     set_role_contextlevels($mrbsviewerid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsviewerid, $context, true);
+    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsviewerid, $context->id, true);
 
     // Create the editor role
     $mrbseditorid = create_role(get_string('mrbseditor', 'block_mrbs'), 'mrbseditor', get_string('mrbseditor_desc', 'block_mrbs'));
     set_role_contextlevels($mrbseditorid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbseditorid, $context, true);
-    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbseditorid, $context, true);
+    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
+    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
 
     // Create the admin role
     $mrbsadminid = create_role(get_string('mrbsadmin', 'block_mrbs'), 'mrbsadmin', get_string('mrbsadmin_desc', 'block_mrbs'));
     set_role_contextlevels($mrbsadminid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsadminid, $context, true);
-    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbsadminid, $context, true);
-    assign_capability('block/mrbs:administermrbs', CAP_ALLOW, $mrbsadminid, $context, true);
-    assign_capability('block/mrbs:viewalltt', CAP_ALLOW, $mrbsadminid, $context, true);
-    assign_capability('block/mrbs:forcebook', CAP_ALLOW, $mrbsadminid, $context, true);
-    assign_capability('block/mrbs:doublebook', CAP_ALLOW, $mrbsadminid, $context, true);
+    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    assign_capability('block/mrbs:administermrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    assign_capability('block/mrbs:viewalltt', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    assign_capability('block/mrbs:forcebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    assign_capability('block/mrbs:doublebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
 
     // Clear any capability caches
     mark_context_dirty($context->path);
