e51317ba12b7369a064d65b9d26e752ade60b35b
Davo Smith <git@davosmith.co.uk>
2014-11-03 10:25:45 +0000

    Fix error when block is uninstalled and then later reinstalled

--- db/install.php
+++ db/install.php
@@ -17,36 +17,45 @@
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_block_mrbs_install() {
-    global $CFG;
+    global $CFG, $DB;
 
-    // Get system context
+    // Get system context.
     if ($CFG->version < 2011120100) {
         $context = get_context_instance(CONTEXT_SYSTEM);
     } else {
         $context = context_system::instance();
     }
 
-    // Create the viewer role
-    $mrbsviewerid = create_role(get_string('mrbsviewer', 'block_mrbs'), 'mrbsviewer', get_string('mrbsviewer_desc', 'block_mrbs'));
-    set_role_contextlevels($mrbsviewerid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsviewerid, $context->id, true);
+    // Create the viewer role.
+    if (!$DB->record_exists('role', array('shortname' => 'mrbsviewer'))) {
+        $mrbsviewerid = create_role(get_string('mrbsviewer', 'block_mrbs'), 'mrbsviewer',
+                                    get_string('mrbsviewer_desc', 'block_mrbs'));
+        set_role_contextlevels($mrbsviewerid, array(CONTEXT_SYSTEM));
+        assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsviewerid, $context->id, true);
+    }
 
-    // Create the editor role
-    $mrbseditorid = create_role(get_string('mrbseditor', 'block_mrbs'), 'mrbseditor', get_string('mrbseditor_desc', 'block_mrbs'));
-    set_role_contextlevels($mrbseditorid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
-    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
+    // Create the editor role.
+    if (!$DB->record_exists('role', array('shortname' => 'mrbseditor'))) {
+        $mrbseditorid = create_role(get_string('mrbseditor', 'block_mrbs'), 'mrbseditor',
+                                    get_string('mrbseditor_desc', 'block_mrbs'));
+        set_role_contextlevels($mrbseditorid, array(CONTEXT_SYSTEM));
+        assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
+        assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbseditorid, $context->id, true);
+    }
 
-    // Create the admin role
-    $mrbsadminid = create_role(get_string('mrbsadmin', 'block_mrbs'), 'mrbsadmin', get_string('mrbsadmin_desc', 'block_mrbs'));
-    set_role_contextlevels($mrbsadminid, array(CONTEXT_SYSTEM));
-    assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
-    assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
-    assign_capability('block/mrbs:administermrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
-    assign_capability('block/mrbs:viewalltt', CAP_ALLOW, $mrbsadminid, $context->id, true);
-    assign_capability('block/mrbs:forcebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
-    assign_capability('block/mrbs:doublebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    // Create the admin role.
+    if (!$DB->record_exists('role', array('shortname' => 'mrbsadmin'))) {
+        $mrbsadminid = create_role(get_string('mrbsadmin', 'block_mrbs'), 'mrbsadmin',
+                                   get_string('mrbsadmin_desc', 'block_mrbs'));
+        set_role_contextlevels($mrbsadminid, array(CONTEXT_SYSTEM));
+        assign_capability('block/mrbs:viewmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+        assign_capability('block/mrbs:editmrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+        assign_capability('block/mrbs:administermrbs', CAP_ALLOW, $mrbsadminid, $context->id, true);
+        assign_capability('block/mrbs:viewalltt', CAP_ALLOW, $mrbsadminid, $context->id, true);
+        assign_capability('block/mrbs:forcebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
+        assign_capability('block/mrbs:doublebook', CAP_ALLOW, $mrbsadminid, $context->id, true);
+    }
 
-    // Clear any capability caches
-    mark_context_dirty($context->path);
+    // Clear any capability caches.
+    $context->mark_dirty();
 }
